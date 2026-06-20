ad7f17ee446f8a528ce488328cf21e64a3640142
UlisesAramis <odiseoaramis@gmail.com>
2026-06-20 16:38:33 -0600

    Feat: add router

--- src/Components/Ui/LanguageSwitcher.tsx
+++ src/Components/Ui/LanguageSwitcher.tsx
@@ -8,18 +8,18 @@ export default function LanguageSwitcher({ lang }: { lang: string }) {
 
   const changeLanguage = () => {
     const newLang = lang === "es" ? "en" : "es";
-
     const newPath = pathname.replace(`/${lang}`, `/${newLang}`);
 
-    router.push(newPath);
+    // Agregamos { scroll: false } para evitar que salte al inicio
+    router.push(newPath, { scroll: false });
   };
 
   return (
     <button
       onClick={changeLanguage}
-      className="cursor-pointer border-2 p-2 rounded-2xl hover:text-emerald-500 "
+      className="cursor-pointer px-5 py-2 rounded-full border border-emerald-500/30 bg-white text-gray-700 font-medium shadow-sm transition-all duration-300 hover:bg-emerald-500 hover:text-white hover:shadow-lg hover:shadow-emerald-500/30 hover:-translate-y-0.5 active:scale-95"
     >
-      {lang === "es" ? "English" : "Español"}
+      {lang === "es" ? "🇺🇸 English" : "🇲🇽 Español"}
     </button>
   );
 }
