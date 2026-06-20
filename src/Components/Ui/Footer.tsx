0f42ecf07028a0298508d9dac0479df6e336cbeb
UlisesAramis <odiseoaramis@gmail.com>
2026-06-20 17:16:59 -0600

    Fix: add height

--- src/Components/Ui/Footer.tsx
+++ src/Components/Ui/Footer.tsx
@@ -29,7 +29,7 @@ const socialLinks: SocialLink[] = [
 
 const Footer = () => {
   return (
-    <footer className="w-full h-20 flex flex-col justify-center items-center">
+    <footer className="w-full h-40 flex flex-col justify-center items-center">
       <hr className="border-t-2 border-emerald-600 w-5/6 mx-auto" />
       <div className="w-5/6 mx-auto flex flex-col items-center pb-5">
         <p className="text-center text-gray-400 py-4">
