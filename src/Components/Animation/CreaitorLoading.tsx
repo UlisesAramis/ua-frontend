66bb51fcf68f5de7a8399ab8b0487e5fa5179ccc
UlisesAramis <odiseoaramis@gmail.com>
2026-06-20 20:17:06 -0600

    fix:fix creaitorloading

--- src/Components/Animation/CreaitorLoading.tsx
+++ src/Components/Animation/CreaitorLoading.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
 import animationData from "@/lottiefiles/stain.json";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 export default function CreaitorLoading() {
   return (
-    <div className="flex justify-center items-center  ">
-      <div className="w-72 h-72 ">
+    <div className="flex justify-center items-center">
+      <div className="w-72 h-72">
         <Lottie animationData={animationData} loop autoplay />
       </div>
     </div>
